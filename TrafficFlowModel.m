6b861ee0e9813bfaa527db8fd326d152a5a6e13b
Anish Muthali <anishmuthali@gmail.com>
2018-04-18 09:15:20 -0700

    Included indexing functions

--- TrafficFlowModel.m
+++ TrafficFlowModel.m
@@ -1,10 +1,14 @@
 v_max = 26.8224;
 rho_max = 3;
-dx = 0.1;
-dt = 0.1;
+dx = 1;
+dt = 1;
 rho_initial = 5;
 rho_final = 10;
 X_total = 5;
 T_total = 5;
+num_T = T_total/dt;
+num_X = X_total/dx;
 
-S = sparse((T_total/dt),(X_total/dx));
+index = @(x,t) x + num_X*t;
+revIndex = @(i) [mod(i,num_X),floor(mod(i, num_T*num_X)/num_X)];
+indexMax = index(X_total,T_total);
