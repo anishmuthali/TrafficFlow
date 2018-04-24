7b5c569cea1c42ad5b6d279e9d213b25c9e1e773
Anish Muthali <anishmuthali@gmail.com>
2018-04-24 09:12:39 -0700

    Finished code, has value errors

--- TrafficFlowModel.m
+++ TrafficFlowModel.m
@@ -1,29 +1,39 @@
-v = 26.8224;
+v_max = 26.8224;
 rho_max = 0.2;
 dx = 1;
 dt = 1;
-rho_initial = 5;
-rho_final = 10;
+rho_initial = 0.8;
+rho_final = 0.8;
 X_total = 5;
 T_total = 5;
 num_T = T_total/dt;
 num_X = X_total/dx;
 
 index = @(x,t) x + num_X*t;
-revIndex = @(i) [mod(i,num_X),floor(mod(i, num_T*num_X)/num_X)];
-rho_0 = @(x) 2;
+revIndex = @(i) [(mod(i,num_X)),(floor(mod(i, num_T*num_X)/num_X))];
+rho_0 = @(x) ((x-2.5)^2);
 indexMax = index(X_total,T_total);
 S = sparse(indexMax);
 B = zeros(indexMax,1);
-for i=0:indexMax
-    [currX,currT] = revIndex(i);
+revIndex(0)
+for i=0:indexMax-1
+    currCoord = revIndex(i);
+    currX = currCoord(1);
+    currT = currCoord(2);
     if currX == 0 || currX == X_total
-        S(i,i) = 1;
-        B(i,1) = rho_initial;
+        S(i+1,i+1) = 1;
+        B(i+1,1) = rho_initial;
     elseif currT == 0
-        S(i,i) = 1;
-        B(i,1) = rho_0(currX);
+        S(i+1,i+1) = 1;
+        B(i+1,1) = rho_0(currX);
     else
-        
+        X_next = index(currX+dx,currT);
+        T_next = index(currX,currT+dt);
+        S(i+1,X_next+1) = -v_max/(2*rho_max*dx);
+        S(i+1,T_next+1) = 1/dt;
+        S(i+1,i+1) = -(1/dt) + v_max/(2*rho_max*dx);
+        B(i+1,1) = 0;
     end
 end
+Rho = S^-1 * B;
+Rho
