59a08e7d9d06fd621588e16b642e9e5b633c7266
Anish Muthali <anishmuthali@gmail.com>
2018-04-26 09:07:57 -0700

    NaN matrix

--- TrafficFlowModel.m
+++ TrafficFlowModel.m
@@ -4,12 +4,8 @@ dx = 1;
 dt = 1;
 rho_initial = 0.15;
 rho_final = 0.15;
-X_total = 2;
-T_total = 2;
-num_T = (T_total/dt);
-num_X = (X_total/dx);
-xn = 2;
-tn = 2;
+xn = 5;
+tn = 5;
 
 index = @(x,t) x + xn*t;
 revIndex = @(i) [mod(i,xn),floor(mod(i, tn*xn)/xn)];
@@ -23,19 +19,27 @@ for i=0:indexMax
     currCoord = revIndex(i);
     currX = currCoord(1);
     currT = currCoord(2);
-    if (currX == 0 || currX == xn-1)
+    if (currX == 0)
         S(i+1,i+1) = 1;
         B(i+1,1) = rho_initial;
+    elseif(currX == xn-1)
+        S(i+1,i+1) = 1;
+        B(i+1,1) = rho_final;
     elseif currT == 0
         S(i+1,i+1) = 1;
         B(i+1,1) = rho_0(currX);
-    else
+    elseif currT ~= tn-1
         X_next = index(currX+dx,currT);
         T_next = index(currX,currT+dt);
+        if(T_next+1>=25)
+            currX
+            currT
+        end
         S(i+1,X_next+1) = -v_max/(2*rho_max*dx);
         S(i+1,T_next+1) = 1/dt;
         S(i+1,i+1) = -(1/dt) + v_max/(2*rho_max*dx);
         B(i+1,1) = 0;
     end
+    
 end
 Rho = S^-1 * B
