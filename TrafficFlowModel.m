571ae8bc24847317598e561de9001ba3e5e01890
Anish Muthali <anishmuthali@gmail.com>
2018-04-30 09:12:06 -0700

    Added comments and deleted useless variables

--- TrafficFlowModel.m
+++ TrafficFlowModel.m
@@ -1,19 +1,17 @@
-v_max = 26.8224;
-rho_max = 0.2;
-dx = 1;
-dt = 1;
-rho_initial = 0.15;
-rho_final = 0.05;
-xn = 5;
-tn = 5;
+v_max = 26.8224; %typical speed on highway (60 mph) in m/s
+rho_max = 0.28; %max car density assuming we have only small Fiat 500s on the freeway
+dx = 1; %distance steps
+dt = 1; %time steps
+rho_initial = 0.2083; %density based on average car length of 4.8 m
+xn = 5; %space of 5 meters
+tn = 5; %time interval of 5 seconds
 
 index = @(x,t) x + xn*t;
 revIndex = @(i) [mod(i,xn),floor(mod(i, tn*xn)/xn)];
-rho_0 = @(x) 0;
+rho_0 = @(x) (sin(x) + 1)/8; %arbitrary function to determine the initial distribution of cars along the stretch of road
 indexMax = index(xn-1,tn-1);
-S = sparse(indexMax+1);
-B = zeros(indexMax+1,1);
-num = 0;
+S = sparse(indexMax+1); %sparse matrix of coefficients
+B = zeros(indexMax+1,1); %matrix for inversing
 for i=0:indexMax
     currCoord = revIndex(i);
     currX = currCoord(1);
@@ -35,4 +33,16 @@ for i=0:indexMax
     end
     
 end
-Rho = S^-1 * B
+Rho = S^-1 * B;
+Rho = reshape(Rho,[5,5]) %reshapes the matrix such that rows signify x-values and columns signify t-values
+
+%Interpretation of results: 
+%--------------------------
+%If there is more space at the end of the freeway, cars will try to go as 
+%fast as they can to fill up that space. Once there is a backup in that 
+%area, cars will move more slowly and they will flow slowly out of the 
+%stretch of freeway, stabilizing the density to a constant value. There is 
+%a fixed density at the start of the freeway, and after a certain amount 
+%of time, the densities throughout the stretch of highway will stabilize 
+%to this value because the amount of cars entering the freeway is held 
+%constant by this initial condition value.
